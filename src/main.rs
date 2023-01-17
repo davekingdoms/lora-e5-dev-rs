e870dc9b3d0e48fecbd3d0cebb955953da5d6985
davekingdoms <regni.alessandro@gmail.com>
2023-01-17 11:19:03 +0100

    Example using pb, led and uart

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@
 
 mod log;
 
+use core::fmt::Write;
+
 use cortex_m::{delay::Delay, interrupt};
 use cortex_m_rt::entry;
 
 use lora_e5_bsp::{
-    hal::{gpio::{PortA, PortB}, pac, util::new_delay},
+    hal::{gpio::{PortA, PortB, pins}, pac, uart::{self, NoRx, Uart1}, util::new_delay},
     led,
     pb::{PushButton, D0},
 };
@@ -30,14 +32,30 @@ fn main() -> ! {
     let mut dp: pac::Peripherals = pac::Peripherals::take().unwrap();
     let cp: pac::CorePeripherals = pac::CorePeripherals::take().unwrap();
 
+
     let gpiob: PortB = PortB::split(dp.GPIOB, &mut dp.RCC);
-    let mut led = interrupt::free(|cs| led::D5::new(gpiob.b5, cs));
+
+    let mut rcc = dp.RCC;
+    let b5 = gpiob.b5;
+    let b6 = gpiob.b6;
+    let usart1 = dp.USART1;
+
+    let mut led = interrupt::free(|cs| led::D5::new(b5, cs));
     led.set_off();
 
-    let gpioa: PortA = PortA::split(dp.GPIOA, &mut dp.RCC);
+    let gpioa: PortA = PortA::split(dp.GPIOA, &mut rcc);
     let button = cortex_m::interrupt::free(|cs| D0::new(gpioa.a0, cs));
 
-    let mut delay: Delay = new_delay(cp.SYST, &dp.RCC);
+    let mut delay: Delay = new_delay(cp.SYST, &rcc);
+    
+
+    rcc.cr.modify(|_, w| w.hsion().set_bit());
+    while rcc.cr.read().hsirdy().is_not_ready() {
+
+    }
+
+    let mut uart: Uart1<NoRx, pins::B6> = interrupt::free(|cs|{ Uart1::new(usart1, 115_200, uart::Clk::Hsi16, &mut rcc).enable_tx(b6, cs)});
+  
 
 
     log::log!("Starting blinky");
@@ -57,8 +75,11 @@ fn main() -> ! {
 
     }
 
+    led.set_on();
+    log::log!("LED is on");
+
     loop {
-        /*delay.delay_ms(10000);
+        /*delay.delay_ms(1000);
         led.set_on();
         log::log!("LED is on");
        
@@ -70,6 +91,9 @@ fn main() -> ! {
             led.toggle();
 
         }
+
+    uart.write_str(">App running..\n\r").unwrap();
+
 
        /*/ delay.delay_ms(10000);
         led.set_off();
